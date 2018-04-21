1973208f3743b74cf76d5556682cad593c8882a7
Alex Vervloet <alexvervloet@Alexs-MacBook-Pro.local>
2018-04-21 16:03:05 +0800

    Feat: Add validation to registration and login, create Profile schema

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const keys = require("../../config/keys");
 
 //Load Input Validation
 const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 //Load User Model
 const User = require("../../models/User");
@@ -31,7 +32,8 @@ router.post("/register", (req, res) => {
 
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json({ errors });
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", // size
@@ -59,17 +61,25 @@ router.post("/register", (req, res) => {
   });
 });
 
-//@route GET api/users/register
-//@desc Register user
+//@route GET api/users/login
+//@desc Login user
 //@access Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   // Find user by email
   User.findOne({ email }).then(user => {
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
 
     //Check password
@@ -93,7 +103,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password incorrect" });
+        errors.password = "Password incorrect";
+        return res.status(400).json(errors);
       }
     });
   });
--- validation/register.js
+++ validation/register.js
@@ -4,8 +4,28 @@ const isEmpty = require("./is-empty");
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
+  dataFields = ["name", "email", "password", "password2"];
+  dataFields.forEach(field => {
+    data[field] = !isEmpty(data[field]) ? data[field] : "";
+    if (Validator.isEmpty(data[field])) {
+      errors[field] = `${field} field is required`;
+    }
+  });
+
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be between 6 and 30 characters";
+  }
+
+  if (!Validator.matches(data.password, data.password2)) {
+    errors.password2 = "Passwords do not match.";
   }
 
   return {
