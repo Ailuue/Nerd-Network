4730b71eacaa4b322c05c37d86904f462255be92
Alex Vervloet <alexvervloet@Alexs-MacBook-Pro.local>
2018-04-21 17:13:53 +0800

    Alpha: Add get routes to view public profile from handle or user id.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -38,6 +38,46 @@ router.get(
   }
 );
 
+//@route GET api/profile/handle/:handle
+//@desc Get profile by handle
+//@access Public
+
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+//@route GET api/profile/user/:user_id
+//@desc Get profile by user ID
+//@access Public
+
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 //@route POST api/profile
 //@desc Create or edit current user's profile
 //@access Private
