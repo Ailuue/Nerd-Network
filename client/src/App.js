794cadeda719403f389a808b33e63629e8b6722e
Alex Vervloet <alexvervloet@Alexs-MacBook-Pro.local>
2018-04-22 17:07:11 +0800

    Alpha: Implement React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Landing from './components/Landing';
@@ -7,11 +9,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
