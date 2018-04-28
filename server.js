7b0aff5f9649b9088f73bdd4d07767f2ca545090
Alex Vervloet <alexvervloet@Alexs-MacBook-Pro.local>
2018-04-28 15:35:08 +0800

    Deploy

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
