7b3e3c3e7820d841ac6899cce42110087f5837b4
Alex Vervloet <alexvervloet@Alexs-MacBook-Pro.local>
2018-04-28 17:10:36 +0800

    Beta: Fix issue when browsers don't have Redux Devtools

--- client/src/App.js
+++ client/src/App.js
@@ -32,8 +32,8 @@ const store = createStore(
   rootReducer,
   state,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
